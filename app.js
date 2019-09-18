65768191467d4a3759052a13fe3ee30f4f066bf3
Ehsan Gharaei <Ehsan.Gharaei@ut.ac.ir>
2019-09-18 14:06:55 +0430

    PORT Bug Fixed

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = process.end.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server Created on ${PORT}`));
